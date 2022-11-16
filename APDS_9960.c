43ce3e231af005b8e7b38e91ccb49b941f65e895
Samuel <samuel.kay@poivron-robotique.fr>
2022-11-16 18:59:46 +0100

    Ajout des fichiers (vides) pour les capteurs apds

---
+++ APDS_9960.c
@@ -0,0 +1,10 @@
+APDS9960_Init(){
+    // Registres à configurer 
+
+
+    // ENABLE (PON) Address : 0x80 - Après les autres registres !!
+    // RESERVED | Gesture En | Prox. Interrupt En | ALS Interrupt En | Wait En | Proximity Detect En |  ALS En | Power On |
+    // Ob0000 0011 => 0x03
+
+
+}
